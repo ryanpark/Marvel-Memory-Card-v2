96888a379cdcfe63c89a838fc023686374daba8d
Ryan <ryan81.park@gmail.com>
2017-01-04 01:09:54 +1100

    RESTful Api

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,24 @@
 import styles from './index.scss';
 import React from 'react';
+import Service from './service.jsx';
 
 export default class App extends React.Component {
+   componentDidMount() { 
+    return fetch(Service.url, {
+        method: 'GET'
+     })
+        .then((res) => res.json()) 
+        .then((data) => {
+          this.setState({'data': data});
+      }).catch(function(err){
+        console.log(err);
+     });
+  }
   render() {
     return (
       <div>
         <h1>It Works!</h1>
-        <p>This React project just works including <span className={styles.redBg}>module</span> local styles.</p>
-        <p>Enjoy!</p>
+        {Service.url}
       </div>
     )
   }
