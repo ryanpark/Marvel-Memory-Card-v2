3a7491dc8e0e430ff0631d4547096578c320f841
Ryan <ryan81.park@gmail.com>
2017-01-08 00:16:54 +1100

    flip card

--- src/card.jsx
+++ src/card.jsx
@@ -4,22 +4,32 @@ import React from 'react';
 export default class Card extends React.Component {
     constructor(props) {
         super(props)
-        this.state = ({
-            click: false
-        });
+        this.state = ({flipped: false})
+        
     }
-    flippCard() {
-      if (this.state.click == false) {
+    resetCards() {
+        console.log(this.props.details)
+    }
+    flippCard() { 
+    
+     if (this.state.flipped == false) {
           this.setState({
-              click : true
+              flipped : true
           })
+         if (this.props.reset == true) {
+            this.resetCards();
+         }
+         return this.props.selectedCard(this.props.detail.id);
+      } else {
+         return false;
       }
-      this.props.selectedCard(this.props.detail.id);
+      
+      return this.props.selectedCard(this.props.detail.id);
     }
-    render() {
+    render() {                                              
         return (
             <li>
-                <div onClick={this.flippCard.bind(this)} className={ (this.state.click == true ? styles.flipped : ' ') + ' '  + styles.card + ' ' + styles.effect__click}>
+                <div onClick={this.flippCard.bind(this)} className={ (this.state.flipped == true ? styles.flipped : ' ') + ' '  + styles.card + ' ' + styles.effect__click}>
                     <div className={styles.card__front}>
                         <span className={styles.card__text}>{this.props.detail.name}</span>
                     </div>
--- src/heros.jsx
+++ src/heros.jsx
@@ -2,37 +2,34 @@ import styles from './index.scss';
 import React from 'react';
 import Card from './card.jsx';
 
-/*To do*/
-let count= 0
-
 export default class Heros extends React.Component {
   constructor(props) {
     super(props)
-    this.state = ({selectedId:''});
+    this.state = ({selectedId: '', primary: false, reset: false});
   }
   resetCard() {
-    
+      alert('no-match')
   }
   selectedCard(id) {
-    count++
-    if (count == 2) {
+    this.setState({selectedId: id});
+    
+    if (this.state.primary == false) {
+        return this.setState({primary: true})
+    } else {
       if (id == this.state.selectedId) {
         alert('match')
       } else {
-        //this.resetCard();
-        //console.log(count)
-        console.log(this.props.list)
-      } 
-      count=0 
+        this.resetCard();
+      }
+      this.setState({primary: false}) 
     }
-    return this.setState({selectedId:id});
   }
   render() {
      const lists = this.props.list.concat(this.props.list).map((e,i) => {
      if ((e.thumbnail.path).includes('image_not_available')) {
        return false;
      }
-       return <Card detail={e} key={i} card={i} selectedCard= {this.selectedCard.bind(this)}/> 
+       return <Card reset={this.state.reset} detail={e} key={i} card={i} selectedCard= {this.selectedCard.bind(this)} ref="cardLists"/> 
     })
     return (
       <div className={styles.cardsContainer}>
